647a59c37bb7dfb3aa65de554e0cac9b0e079d6a
mgastonportillo <mgastonportillo@gmail.com>
2022-11-26 21:34:08 -0300

    Fix carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,37 +1,44 @@
 /* eslint-disable react/prop-types */
-import '../../Styles/Carousel.scss'
 import React from 'react'
 import { useState } from 'react'
 import Imgs from '../../json/CarouselImgs.json'
 import { Image } from './Image'
+import '../../Styles/Carousel.scss'
 
-const DotBtn = ({ id, active }) => {
- 
-  return (
-    <li className={`dot ${active ? 'active' : ''}`}>{id}</li>
-  )
+const DotBtn = ({ id, active, handler }) => {
+	return (
+		<li className={`dot ${active === id ? 'active' : ''}`} onClick={handler}>
+			{id}
+		</li>
+	)
 }
 
+const Carousel = () => {
+	const [active, setActive] = useState()
 
-export const Carousel = () => {
-  const [active, setActive] = useState(false)
+	const btnClick = (id) => {
+		setActive(id)
+	}
 
-  const btnClick = () => {
-    setActive(!active)
-  }
-  
-  return (
-    <div className='Carousel'>
-      <div className='CarouselImgs'>
-        {
-          Imgs.map((x, index) => <Image key={index} src={x.URL} alt={x.Alt} />)
-        }
-      </div>
-      <ul className='CarouselDots'>
-        {
-          Imgs.map(x => <DotBtn key={x.Id} id={x.Id} active={active} onClick={btnClick} />)
-        }
-      </ul>
-    </div>
-  )
+	return (
+		<div className='Carousel'>
+			<div className='CarouselImgs'>
+				{Imgs.map((x, index) => (
+					<Image key={index} src={x.URL} alt={x.Alt} />
+				))}
+			</div>
+			<ul className='CarouselDots'>
+				{Imgs.map((x) => (
+					<DotBtn
+						key={x.Id}
+						id={x.Id}
+						active={active}
+						handler={() => btnClick(x.Id)}
+					/>
+				))}
+			</ul>
+		</div>
+	)
 }
+
+export default Carousel
