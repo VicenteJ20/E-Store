f0ccdeb5f5bbc99c376c70628ef502c9c35872bf
Cata <catalina.zapata@usach.cl>
2022-12-07 13:11:12 -0300

    V0R3: repairing bug

---
+++ src/components/cart/cart.jsx
@@ -0,0 +1,8 @@
+<div className='navbar__shoppingCartIcon'>
+<i className='navbar__shoppingCartIcon__icon'>
+    <BsCartPlus />
+</i>
+<a className='navbar__icon__title' href='#'>
+    Carrito
+</a>
+</div>
